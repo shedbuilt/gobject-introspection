ec5ed8c400611bddeb517f4cc089812e30eecd5a
Auston Stewart <auston@shedbuilt.net>
2018-10-25 20:45:40 -1000

    Updating to 1.58.0

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Configure
 ./configure --prefix=/usr    \
             --disable-static \
-            --with-python=/usr/bin/python3 && \
-make -j $SHED_NUM_JOBS && \
+            --with-python=/usr/bin/python3 &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
